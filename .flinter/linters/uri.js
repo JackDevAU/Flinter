9e5deaa02ab7647ce8d6cb143da2269e435c8fb2
Brady Stroud <bradystroud@ssw.com.au>
2022-08-19 17:18:19 +1000

    aerg

--- .flinter/linters/uri.js
+++ .flinter/linters/uri.js
@@ -3,19 +3,20 @@
  * @returns {{result: boolean, error?: string}}
  */
 export async function run(params) {
-  console.log("TEST CUSTOM RULE");
+  console.log('TEST CUSTOM RULE');
   if (!params.value) {
     return {
       result: false,
-      error: "Uri is required",
+      error: 'Uri is required',
     };
   }
 
-  var uriRegex = new RegExp("(?:($.*):)");
+  var uriRegex = new RegExp('(?:($.*):)');
+  console.log(uriRegex.test(params.value));
   if (!uriRegex.test(params.value)) {
     return {
       result: false,
-      error: "Uri must be kebab case",
+      error: 'Uri must be kebab case',
     };
   }
 
